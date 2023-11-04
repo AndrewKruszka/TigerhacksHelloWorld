94951e0a224d9c144de14e3b7a01cdced57e1284
AndrewKruszka <andkruszka@gmail.com>
2023-11-04 15:15:16 -0500

    Some edits

--- HelloWorldApp/HelloWorldApp/Views/emojiPopup.swift
+++ HelloWorldApp/HelloWorldApp/Views/emojiPopup.swift
@@ -9,33 +9,35 @@ import SwiftUI
 
 struct emojiPopup: View {
     var body: some View {
-        ZStack {
-            Color.green
-                .ignoresSafeArea()
-                .brightness(0.15)
-            VStack {
-                Group{
+        NavigationStack{
+            ZStack {
+                Color.green
+                    .ignoresSafeArea()
+                    .brightness(0.15)
+                VStack {
+                    Group{
                     Text("Emoji Logo")
                         .font(.largeTitle)
                         .frame(width: 200, height: 40)
                         .foregroundColor(.white)
                     
-                    Button(action: {
-                        print("tapped!")
-                    }, label: {
-                        Text("Continue")
-                            .font(.title2)
-                            .foregroundColor(.white)
-                            .frame(width: 150, height: 75)
-                            .background(Color.mint)
-                            .cornerRadius(75)
-                            .padding()
-                    })
-                }.frame(maxHeight: .infinity, alignment: .bottom)
+                        NavigationLink { selfieView() }
+                            label: {
+                                Text("Continue")
+                                    .font(.title2)
+                                    .foregroundColor(.white)
+                                    .frame(width: 150, height: 75)
+                                    .background(Color.mint)
+                                    .cornerRadius(75)
+                                    .padding()
+                            }
+                        }
+                    }
             }
         }
     }
 }
+
 
 #Preview {
     emojiPopup()
---
+++ HelloWorldApp/HelloWorldApp/Views/selfie.swift
@@ -0,0 +1,20 @@
+//
+//  selfie.swift
+//  HelloWorldApp
+//
+//  Created by Andrew Kruszka on 11/4/23.
+//
+
+import SwiftUI
+
+struct selfieView: View {
+    
+    var body: some View {
+        Color.gray
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    selfieView()
+}
