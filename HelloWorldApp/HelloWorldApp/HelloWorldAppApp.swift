e5a8c4b1a86cb78c8a4a176f2df47c8e9208fed2
AndrewKruszka <andkruszka@gmail.com>
2023-11-04 00:20:48 -0500

    Initial
    Initial check in

---
+++ HelloWorldApp/HelloWorldApp/HelloWorldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppApp.swift
+//  HelloWorldApp
+//
+//  Created by Andrew Kruszka on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
